a32ca37befc6d58038f7ab044eb075d382ea87cb
HieuNV <hieunv@live.com>
2017-01-23 08:36:54 +0700

    [CS] regex

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 export const DEFAULT_BASE_URL = 'https://chatpp.thangtd.com/img/emoticons/';
 
+export const MATCH_REGEX_ENCODE = /[-\/\\^$*+?.()|[\]{}]/g;
+
 export const Html = window.Html || {};
 
 Html.encode = (html) => {
@@ -10,6 +12,11 @@ Html.decode = (html) => {
   const e = document.createElement('div');
   e.innerHTML = html;
   return e.textContent;
+};
+
+export const RegExpFactory = window.RegExpFactory || {};
+RegExpFactory.create = (regex) => {
+  return new RegExp(Html.encode(regex.replace(MATCH_REGEX_ENCODE, '\\$&')), 'g');
 };
 
 export const MATCH_STICKER_TAG = /\[sticker(.*?)\[\/sticker]/g;
